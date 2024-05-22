2e1801458aabbcee9cb3eefbbe41a07621af37a7
Tirkey <61328112+TIRK3Y@users.noreply.github.com>
2024-05-23 01:47:32 +0530

    Add files via upload

---
+++ ifElse.js
@@ -0,0 +1,15 @@
+prompt("Your Name: ");
+prompt("Their Name: ");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if (loveScore > 70){
+  alert("Your love score is " + loveScore + "% Bahut Pyarr!");
+}
+else
+{
+  alert("Your love score is " + loveScore + "% Leh bhaii yahi hai tera kmm pyar");
+}
+
+alert("Your love score is " + loveScore + "%");
---
+++ loveCalculator.js
@@ -0,0 +1,6 @@
+prompt("Your Name: ");
+prompt("Their Name: ");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+alert("Your love score is " + loveScore +"%");
